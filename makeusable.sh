26ff85f6fa57d1cddeb9ced169d88d913ef7cf5c
Michael Schönitzer <michael@schoenitzer.de>
2012-01-31 15:21:45 +0100

    fixed bug with non-integer-data of allensbach

--- makeusable.sh
+++ makeusable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in readable/*; do
  cat $i | grep -v \# | grep -v ^$ | tr '\t' . | \
-  awk -F. '{ print ($3-2011)*360+($2-1)*30+$1,$4 }' | \
+  awk -F. '{ print ($3-2011)*360+($2-1)*30+$1,$4+0.1*$5 }' | \
   tr ' ' '\t' > useable/${i/readable\//};
 done

c3fe40bdfcf1245a11018b146de254d914bb3793
Michael F. Schönitzer <michael@schoenitzer.de>
2012-04-07 20:11:14 +0200

    remove *~-files

--- makeusable.sh
+++ makeusable.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm readable/*~ 2> /dev/null
+rm useable/*~ 2> /dev/null
 for i in readable/*; do
  cat $i | grep -v \# | grep -v ^$ | tr '\t' . | \
   awk -F. '{ print ($3-2011)*360+($2-1)*30+$1,$4+0.1*$5 }' | \

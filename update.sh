14e869c7d6d9ce3e131836989ab94f2b6c46673e
Michael F. Schönitzer <michael@schoenitzer.de>
2012-04-23 16:41:26 +0200

    fixed small bug

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ cp *png online
 cp *svg online
 read -p "Bearbeitungskomentar: " comment
 git commit -a -m "$comment"
-git push
+git push origin master
 cd online
 git commit -a -m "$comment"
 git push origin gh-pages
